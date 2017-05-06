88848aba8a2f8482ae81dd2e3a59ab4d849f188b
cale <cale.bradbury@gmail.com>
2017-05-06 18:43:30 -0400

    edited cave material, lights flicker through a gradient of colors

--- TOJam12/Assets/Scripts/Inventory.cs
+++ TOJam12/Assets/Scripts/Inventory.cs
@@ -6,6 +6,7 @@ public class Inventory : MonoBehaviour {
 
     public List<GameObject> inventory;
     public int space = 16;
+    public float animateInTime = .5f;
 
     public bool hasSpace
     {
@@ -20,8 +21,22 @@ public class Inventory : MonoBehaviour {
     {
         inventory.Add(g);
         g.transform.parent = transform;
-        g.transform.localPosition = Vector3.zero;
-        g.transform.localEulerAngles = Vector3.zero;
         g.GetComponent<Collider>().enabled = false;
+        StartCoroutine(AnimateToInventory(g, animateInTime));
+    }
+
+    IEnumerator AnimateToInventory(GameObject g, float time)
+    {
+        float step = 1f / time;
+        float t = 1;
+        Vector3 startPos = g.transform.localPosition;
+        while (t > 0)
+        {
+            t -= step*Time.deltaTime;
+            t = Mathf.Max(t, 0);
+            g.transform.localScale = Vector3.one * t;
+            g.transform.localPosition = Vector3.Lerp(Vector3.zero, startPos, Mathf.Pow(t, 2));
+            yield return new WaitForEndOfFrame();
+        }
     }
 }
